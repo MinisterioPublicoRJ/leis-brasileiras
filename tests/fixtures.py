053bb1823225ed9eb76a0abef2a07164cbb90e71
bernardoccordeiro <bernardoccordeiro@gmail.com>
2020-01-08 16:56:39 -0300

    Remove unused fixtures

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,29 +1,3 @@
-# Fixtures clean_author_name
-
-author_name_1 = 'Vereador  Dr.Fulâno '
-expected_author_name_1 = 'DR. FULANO'
-
-author_name_2 = 'Vereadora Coisinha'
-expected_author_name_2 = 'COISINHA'
-
-# Fixtures expand_results
-
-names_dict = [
-    {'col1': 1, 'col2': 'nome1,nome2'},
-    {'col1': 2, 'col2': 'nome1'}
-]
-expected_expanded_names_dict = [
-    {'col1': 1, 'col2': 'nome1'},
-    {'col1': 1, 'col2': 'nome2'},
-    {'col1': 2, 'col2': 'nome1'}
-]
-
-# Fixtures get_from_depara
-
-depara = [
-    {'nome_camara': 'DR. FULANO', 'nome_outro': 'FULANO'}
-]
-
 # Fixtures extract_projeto
 
 string_projeto_lei_1 = 'Projeto de Lei nº 231A/2019'
